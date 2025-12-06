e1c8b0d9d06bc385972426f33e3444f8ea8d6f20
saleem <ab588538@gmail.com>
2025-12-06 23:31:49 +0530

    Update API base URL and gitignore

--- edutech/src/Api.jsx
+++ edutech/src/Api.jsx
@@ -1,9 +1,9 @@
-import axios from "axios"
+import axios from "axios";
 
-export const BASE_URL = "http://localhost:8000/api/v1"
+export const BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-    baseURL:BASE_URL
-})
+  baseURL: BASE_URL,
+});
 
-export default api
+export default api;
